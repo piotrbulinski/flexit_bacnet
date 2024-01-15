f7d585dd3d1c00ab124f5bbac8cf164a96c834a0
Jonas Fors Lellky <lellky@gmail.com>
2024-01-15 10:54:14 +0100

    Refacors device name and shorten the model identifier

--- flexit_bacnet/nordic.py
+++ flexit_bacnet/nordic.py
@@ -166,19 +166,23 @@ DEVICE_PROPERTIES = [
 
 # The first six digits in the Nordic serial number corresponds to the Nordic model.
 NORDIC_MODELS = {
-    800111: "Nordic S2 REL",
-    800121: "Nordic S3 REL",
-    800110: "Nordic S2 RER",
-    800120: "Nordic S3 RER",
-    800221: "Nordic CL4 REL",
-    800220: "Nordic CL4 RER",
-    800130: "Nordic S4 RER",
-    800131: "Nordic S4 REL",
-    800210: "Nordic CL2 RER",
-    800211: "Nordic CL2 REL",
-    800200: "Nordic CL3 RER",
-    800201: "Nordic CL3 REL",
-    800300: "Nordic KS3 RER",
-    800301: "Nordic KS3 REL",
+    800111: "S2 REL",
+    800121: "S3 REL",
+    800110: "S2 RER",
+    800120: "S3 RER",
+    800221: "CL4 REL",
+    800220: "CL4 RER",
+    800130: "S4 RER",
+    800131: "S4 REL",
+    800210: "CL2 RER",
+    800211: "CL2 REL",
+    800200: "CL3 RER",
+    800201: "CL3 REL",
+    800300: "KS3 RER",
+    800301: "KS3 REL",
 }
 
+# The name of the device
+DEVICE_NAMES = {
+    "HvacFnct21y_A": "Flexit Nordic",
+}
